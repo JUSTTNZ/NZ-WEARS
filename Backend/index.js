ebffe5cecfa8ff5f77fbd9db9a8977363dfcec88
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-08-23 06:58:55 +0100

    BACKEND

---
+++ Backend/index.js
@@ -0,0 +1,138 @@
+const port = 4000;
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
+const { type } = require('os');
+
+
+app.use(express.json());
+app.use(cors());
+
+// Database Connection with  MongoDB
+mongoose.connect('mongodb+srv://NZDdev:Belligerency01$@cluster0.prny1.mongodb.net/e-commerce');
+
+//API Creation
+
+app.get('/', (req,res) => {
+    res.send('Express App is running')
+})
+
+// Image Storage Engine
+
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename:(req,file,cb)=> {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage:storage})
+
+//Creating Upload Endpoint for image
+app.use('/images', express.static('upload/images'))
+
+app.post('/upload', upload.single('product'), (req,res)=> {
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+// sCHEMA for Creating Products
+
+const Product = mongoose.model('Product', {
+    id:{
+        type: Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image: {
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    available: {
+        type:Boolean,
+        default:true,
+    },
+
+})
+
+app.post('/addproduct',async (req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length>0)
+    {
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }    
+    else
+    {
+        id=1;
+    }
+    const product = new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    })
+    console.log(product);
+    await product.save();
+    console.log('saved');
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+//creating API for deleting product
+
+app.post('/removeproduct', async (req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log('Removed');
+    res.json({
+        success: true,
+        name:req.body.name
+    })
+})
+
+// Creating API for getting all products
+app.get('/allproducts', async (req,res)=>{
+    let products = await Product.find({});
+    console.log("All product Fetched");
+    res.send(products);
+})
+
+app.listen(port, (error)=>{
+    if (!error) {
+        console.log('server Running on port '+port)
+    }
+    else {
+        console.log('Error : '+error)
+    }
+})
