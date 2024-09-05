9eb2d52e579a72a2460cf5254d5e2cbf92142ddf
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-09-05 11:50:17 +0100

    updated changes

--- Backend/index.js
+++ Backend/index.js
@@ -25,8 +25,8 @@ app.get('/', (req,res) => {
 
 const storage = multer.diskStorage({
     destination: './upload/images',
-    filename:(req,file,cb)=> {
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    filename:(req,file,callback)=> {
+        return callback(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
     }
 })
 
@@ -219,6 +219,51 @@ app.get('/popularinwomen', async(req,res)=>{
     let popular_in_women = products.slice(0,4);
     console.log("popular in women fetched")
     res.send(popular_in_women);
+})
+
+// creating middleware to fetch user
+    const fetchUser = async (req,res,next)=>{
+        const token = req.header('auth-token');
+        if(!token){
+            res.status(401).send({errors:"please authenticate using valid token"})
+        }
+        else{
+            try {
+                const data = jwt.verify(token,'secret_ecom');
+                req.user = data.user;
+                next();
+            } catch (error) {
+                res.status(401).send({errors:"please authenticate using a valid token"})
+            }
+        }
+    }
+
+// creating endpoint for adding products in cartdata
+app.post('/addtocart',fetchUser, async(req,res)=>{
+    console.log("added",req.body.itemId);
+   // console.log(req.body,req.user);
+   let userData = await Users.findOne({_id:req.user.id});
+   userData.cartData[req.body.itemId] += 1;
+   await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData})
+   res.send("Added")
+})
+
+// creating endpoint for adding products in cartdata
+app.post('/removefromcart',fetchUser, async(req,res)=>{
+    console.log("removed",req.body.itemId);
+    let userData = await Users.findOne({_id:req.user.id});
+    if(userData.cartData[req.body.itemId]>0)
+
+        userData.cartData[req.body.itemId] -= 1;
+        await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData})
+        res.send("removed")
+    
+})
+
+app.post('/getcart',fetchUser,async (req,res)=>{
+    console.log("Get cart");
+    let userData = await Users.findOne({_id:req.user.id})
+    res.json(userData.cartData);
 })
 app.listen(port, (error)=>{
     if (!error) {
--- Frontend/NZ/src/Context/ShopContext.jsx
+++ Frontend/NZ/src/Context/ShopContext.jsx
@@ -19,18 +19,59 @@ const ShopContextProvider = (props) => {
     useEffect(()=>{
        fetch('http://localhost:4000/allproducts')
        .then((response)=>response.json())
-       .then((data)=>setAll_Product(data))
+       .then((data)=>setAll_Product(data));
+
+       if(localStorage.getItem('auth-token')){
+            fetch('http://localhost:4000/getcart',{
+                method:'POST',
+                headers:{
+                    Accept:'application/form-data',
+                    'auth-token':`${localStorage.getItem('auth-token')}`,
+                    'Content-Type':'application/json',
+                },
+                body:"",
+            }).then((response)=>response.json())
+              .then((data)=>setCartItems(data))
+       }
     },[])
 
     
     const addToCart = (itemId) => {
         setCartItems((prev) => ({...prev,[itemId]:prev[itemId]+1}))
+        if(localStorage.getItem('auth-token')){
+            fetch('http://localhost:4000/addtocart',{
+                method: 'POST',
+                headers:{
+                    Accept:'application/form-data',
+                    'auth-token':`${localStorage.getItem('auth-token')}`,
+                    'Content-Type':'application/json',
+                },
+                body:JSON.stringify({"itemId":itemId}),
+            })
+            .then((response)=>response.json())
+            .then((data)=>console.log(data));
+        }
         // console.log("Product added to cart:", itemId);
         // console.log(cartItems)  
     }
 
+
+
     const removeFromCart = (itemId) => {
         setCartItems((prev) => ({...prev,[itemId]:prev[itemId]-1}))
+        if(localStorage.getItem('auth-token')){
+            fetch('http://localhost:4000/removefromcart',{
+                method: 'POST',
+                headers:{
+                    Accept:'application/form-data',
+                    'auth-token':`${localStorage.getItem('auth-token')}`,
+                    'Content-Type':'application/json',
+                },
+                body:JSON.stringify({"itemId":itemId}),
+            })
+            .then((response)=>response.json())
+            .then((data)=>console.log(data));
+        }
     }
 
     const getTotalCartAmount = () => {
