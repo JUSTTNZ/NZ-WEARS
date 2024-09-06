a29c3292cfb5b492ca5f5eb1222c563388b97651
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-09-06 07:56:37 +0100

    backend image changes

--- Backend/index.js
+++ Backend/index.js
@@ -47,7 +47,7 @@ app.use('/images', express.static('upload/images'))
 app.post('/upload', upload.single('product'), (req,res)=> {
     res.json({
         success:1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
+        image_url:`https://nz-wears-su6a.vercel.app/images/${req.file.filename}`
     })
 })
 
